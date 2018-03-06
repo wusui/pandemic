a8a12c7892252c8950277e03b78f2255a0d5a1ad
Warren Usui <warrenusui@earthlink.net>
2018-03-06 00:50:08 -0800

    Add passing of cards.

--- src/clickPlayer.js
+++ src/clickPlayer.js
@@ -5,7 +5,30 @@ var clickPlayer = function() {
             return
         }
         if (info.misc.card_played >= 0) {
-            alert('card passing '+citymap.bynumb[info.misc.card_played]+" to "+action.toString())
+            var iam = info.players.plyr_move
+            var mloc = info.players.plist[iam].xlocation
+            var oloc = info.players.plist[action].xlocation
+            if (mloc != oloc) {
+                return
+            }
+            var takeok = false
+            if (mloc == info.misc.card_played) {
+                takeok = true
+            }
+            else {
+                if (info.players.plist[iam].name == 'R') {
+                    takeok = true
+                }
+            }
+            if (takeok) {
+                alert('card passing '+citymap.bynumb[info.misc.card_played]+" to "+action.toString())
+                var fromguy = info.players.plist[iam].cards
+                fromguy.splice(fromguy.indexOf(info.misc.card_played),1)
+                var toguy = info.players.plist[action]
+                toguy.cards.push(info.misc.card_played)
+                info.misc.card_played = -1
+                handleInput.update_page(info)
+            }
         }
         if (info.players.plist[info.players.plyr_move].name == 'D') {
             info.misc.dispatched_player = action
